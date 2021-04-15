96c59b3f845ae02074f973264e7873651a44e7ce
HERMAN <toto23630826@gmail.com>
2021-04-15 12:06:56 +0800

    SpotLight with bug

--- src/scene/light.h
+++ src/scene/light.h
@@ -59,4 +59,44 @@ protected:
 	double quadAtten;
 };
 
+// FROM spotlight.ray in demo
+//spot_light {
+//position = (0, 2, 0);
+//direction = (0, -2, 0);
+//edgeplace = (30, 0, 0);
+//colour = (0.8, 0.8, 0.8);
+//}
+//spot_light{
+//	position = (1, 2, -1);
+//	direction = (-1, -2, 1);
+//	edgeplace = (20, 0, 0);
+//	colour = (0.8, 0.8, 0.8);
+//}
+
+class SpotLight
+	: public Light
+{
+public:
+	SpotLight(Scene* scene, const vec3f& pos, const vec3f& color, const vec3f& d, const vec3f& ep)
+		: Light(scene, color), position(pos), direction{ d }, edgeplace{ ep } {
+		angle = edgeplace[0];
+		constantAtten = 0.0;
+		linearAtten = 0.0;
+		quadAtten = 0.25;
+	}
+	virtual vec3f shadowAttenuation(const vec3f& P) const;
+	virtual double distanceAttenuation(const vec3f& P) const;
+	virtual vec3f getColor(const vec3f& P) const;
+	virtual vec3f getDirection(const vec3f& P) const;
+
+protected:
+	vec3f position;
+	vec3f direction;
+	vec3f edgeplace;
+	double angle;
+	double constantAtten;
+	double linearAtten;
+	double quadAtten;
+};
+
 #endif // __LIGHT_H__
