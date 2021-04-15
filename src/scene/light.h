04977bb5580cd0844345ef06d9cd54f7ace968b5
HERMAN <toto23630826@gmail.com>
2021-04-15 19:36:26 +0800

    Implemented Warn Model

--- src/scene/light.h
+++ src/scene/light.h
@@ -59,20 +59,6 @@ protected:
 	double quadAtten;
 };
 
-// FROM spotlight.ray in demo
-//spot_light {
-//position = (0, 2, 0);
-//direction = (0, -2, 0);
-//edgeplace = (30, 0, 0);
-//colour = (0.8, 0.8, 0.8);
-//}
-//spot_light{
-//	position = (1, 2, -1);
-//	direction = (-1, -2, 1);
-//	edgeplace = (20, 0, 0);
-//	colour = (0.8, 0.8, 0.8);
-//}
-
 class SpotLight
 	: public Light
 {
@@ -97,30 +83,55 @@ protected:
 	double quadAtten;
 };
 
-//class WarnModelLight
-//	: public Light
-//{
-//public:
-//	SpotLight(Scene* scene, const vec3f& pos, const vec3f& color, const vec3f& d, const vec3f& ep)
-//		: Light(scene, color), position(pos), direction{ d }, edgeplace{ ep } {
-//		angle = edgeplace[0];
-//		constantAtten = 0.0;
-//		linearAtten = 0.0;
-//		quadAtten = 0.25;
-//	}
-//	virtual vec3f shadowAttenuation(const vec3f& P) const;
-//	virtual double distanceAttenuation(const vec3f& P) const;
-//	virtual vec3f getColor(const vec3f& P) const;
-//	virtual vec3f getDirection(const vec3f& P) const;
-//
-//protected:
-//	vec3f position;
-//	vec3f direction;
-//	vec3f edgeplace;
-//	double angle;
-//	double constantAtten;
-//	double linearAtten;
-//	double quadAtten;
-//};
+class WarnModel
+	: public Light
+{
+public:
+	WarnModel(Scene* scene, const vec3f& pos, const vec3f& color, const vec3f& d, const double& sz, const double& sp)
+		: Light(scene, color), position(pos), direction{ d }, size{ sz } {
+		shape = sp;
+		constantAtten = 0.0;
+		linearAtten = 0.0;
+		quadAtten = 0.25;
+
+		vec3f t = { 0, 1, 0 };
+		vec3f X = direction.cross(t).normalize();
+		vec3f Y = direction.cross(X).normalize();
+		X = direction.cross(Y).normalize();
+
+		mat4f translation({ 1, 0, 0, -position[0] },
+						{ 0, 1, 0, -position[1] },
+						{ 0, 0, 1, -position[2] },
+						{ 0, 0, 0, 0 });
+
+		mat4f rotation({ X[0], X[1], X[2], 0 },
+						{ Y[0], Y[1], Y[2], 0 },
+						{ direction[0], direction[1], direction[2], 0 },
+						{ 0, 0, 0, 1 });
+
+		mat4f projection({ 1, 0, 0, 0 },
+						{ 0, 1, 0, 0 },
+						{ 0, 0, 1, 0 },
+						{ 0, 0, 1, 0 });
+
+		matrix = projection * rotation * translation;
+	}
+	virtual vec3f shadowAttenuation(const vec3f& P) const;
+	virtual double distanceAttenuation(const vec3f& P) const;
+	virtual vec3f getColor(const vec3f& P) const;
+	virtual vec3f getDirection(const vec3f& P) const;
+
+protected:
+	vec3f position;
+	vec3f direction;
+	double size;
+	int shape;
+
+	mat4f matrix;
+	
+	double constantAtten;
+	double linearAtten;
+	double quadAtten;
+};
 
 #endif // __LIGHT_H__
