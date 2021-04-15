e09e66ba41fcd0f6ab53885362fb70fa57065006
HERMAN <toto23630826@gmail.com>
2021-04-16 01:27:51 +0800

    Implemented Texture Mapping

--- src/SceneObjects/Sphere.cpp
+++ src/SceneObjects/Sphere.cpp
@@ -2,6 +2,8 @@
 
 #include "Sphere.h"
 
+#define PI 3.14159265359
+
 bool Sphere::intersectLocal( const ray& r, isect& i ) const
 {
 	vec3f v = -r.getPosition();
@@ -34,3 +36,39 @@ bool Sphere::intersectLocal( const ray& r, isect& i ) const
 	return true;
 }
 
+// Inverse Mapping in lecture notes
+vec3f Sphere::mapTexture(const vec3f& intersection, unsigned char* texture, const int& width, const int& height) const
+{
+	// Sp, Se, Sn
+	vec3f sp = { 0, 1, 0 };
+	vec3f se = { 1, 0, 0 };
+	vec3f sn = transform->globalToLocalCoords(intersection).normalize();
+
+	double u, v;
+
+	// v
+	double phi = acos(-sn * sp);
+	v = phi / PI;
+
+	// u
+	if (v >= 1 || v <= 0) {
+		u = 0;
+	}
+	else {
+		double theta = acos((se * sn) / (sin(phi))) / (2 * PI);
+		if ((sp.cross(se)) * sn > 0) {
+			u = theta;
+		}
+		else {
+			u = 1 - theta;
+		}
+	}
+
+	// UV2TextureMap
+	int x = min(int(u * width), width - 1);
+	int y = min(int(v * height), height - 1);
+
+	int r = (y * width + x) * 3, g = (y * width + x) * 3 + 1, b = (y * width + x) * 3 + 2;
+
+	return vec3f{ texture[r] / 255.0f, texture[g] / 255.0f, texture[b] / 255.0f };
+}
